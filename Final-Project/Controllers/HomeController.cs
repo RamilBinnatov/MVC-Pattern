5fafc377c675123f54aadf74bdec9e2471ec5e86
RamilBinnatov <ramilrb@code.edu.az>
2022-12-29 13:15:48 +0400

    MVC-Pattern

---
+++ Final-Project/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Final_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
